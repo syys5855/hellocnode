dc6b9b1e4fb8ba969b76419938667a1638bebf46
syys <1028824533@qq.com>
2018-02-09 14:57:04 +0800

    banner 切换

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -2,7 +2,7 @@
 <div>
     <div class="banner-wrap">
         <div class="banner">
-            <div class="banner-item" :class='[item.class,{active:item.index===active,pre:item.index===(active-1)%len,next:item.index===(active+1)%len}]' v-for="(item,index) in items" :key='index'></div>
+            <div class="banner-item" :class='[item.class,{active:item.index===active,pre:item.index===getIndex(active-1),next:item.index===getIndex(active+1)}]' v-for="(item,index) in bannerItems" :key='index'></div>
         </div>
     </div>
     <button @click="clickNext">btnNext</button>
@@ -18,26 +18,46 @@ export default {
       items: [
         {
           class: "red",
-          value: 1,
-          index: 0
+          value: 1
         },
         {
           class: "green",
-          value: 2,
-          index: 1
+          value: 2
         },
         {
           class: "yellow",
-          value: 3,
-          index: 2
+          value: 3
+        },
+        {
+          class: "black",
+          value: 4
+        },
+        {
+          class: "orange",
+          value: 5
         }
       ],
-      active: 0
+      active: 0,
+      branch: 1
     };
   },
   computed: {
     len() {
       return this.items.length;
+    },
+    bannerItems() {
+      return this.items.map((item, index) => {
+        return Object.assign(item, { index });
+      });
+
+      let arr = [],
+        active = this.active,
+        branch = this.branch;
+      for (let st = active - branch, ed = active + branch; st <= ed; st++) {
+        let index = this.getIndex(st);
+        arr.push(Object.assign(this.items[index], { index }));
+      }
+      return arr;
     }
   },
   methods: {
@@ -48,12 +68,14 @@ export default {
       return descript;
     },
     clickNext() {
-      this.active = ++this.active % this.items.length;
+      this.active = this.getIndex(this.active + 1);
     },
     clickPre() {
-      let active = this.active - 1;
-      active = active % this.items.length;
-      this.active = active < 0 ? this.items.length + active : active;
+      this.active = this.getIndex(this.active - 1);
+    },
+    getIndex(index) {
+      let i = index % this.items.length;
+      return i < 0 ? this.items.length + i : i;
     }
   }
 };
@@ -62,26 +84,36 @@ export default {
 <style scoped lang="scss">
 .red {
   background-color: red;
-  background: url("../asset/777777.png") center no-repeat;
+  //   background: url("../asset/777777.png") center no-repeat;
 }
 .green {
   background-color: green;
-  background: url("../asset/888888.png") center no-repeat;
+  //   background: url("../asset/888888.png") center no-repeat;
 }
 .yellow {
   background-color: yellow;
-  background: url("../asset/999999.png") center no-repeat;
+  //   background: url("../asset/999999.png") center no-repeat;
+}
+.black {
+  background-color: black;
+  //   background: url("../asset/999999.png") center no-repeat;
+}
+.orange{
+    background-color: orange;
 }
 .active {
   transform: translate3d(0, 0, 100px) !important;
   transform-origin: center bottom;
   left: 20vw !important;
+  opacity: 1 !important;
 }
 .pre {
   left: 0vw !important;
+  opacity: 1 !important;
 }
 .next {
   left: 40vw !important;
+  opacity: 1 !important;
 }
 
 .banner-wrap {
@@ -99,11 +131,12 @@ export default {
 }
 .banner-item {
   position: absolute;
-  left: 0;
+  left: 20vw;
   top: 0;
   width: 60vw;
   height: 100px;
-  transition: all 0.2s ease-in-out;
   transform: translate3d(0, 0, 0px);
+  transition: all 0.3s ease-in-out;
+  //   opacity: 0;
 }
 </style>
